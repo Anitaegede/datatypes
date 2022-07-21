93d0af72d4da565b3479d357ca8e3efd2be51f94
Anitaegede <debbyat2008@gmail.com>
2022-07-21 13:56:10 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("Hello")
+let n="Anita"
+let surname="Egede"
+let two=n+" "+surname                    
+console.log(two)
+let x=2
+let y=4
+let add=x+y
+let sub=y-x
+let div=y/x
+let mul=y*x
+console.log(mul)
+console.log(add)
+console.log(sub)
+console.log(div)
+let equals= x==y
+console.log(equals)
+let notequals=x!=y
+console.log(notequals)
+let greaterthan=x>y
+console.log(greaterthan)
+let lessthan=x<y
+console.log(lessthan)
+let win
+console.log(win)
