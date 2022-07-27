f7fbd27cfb5aba225cb639ea99baa3b91e0db134
Anitaegede <debbyat2008@gmail.com>
2022-07-27 11:42:01 +0100

    new

--- index.js
+++ index.js
@@ -23,3 +23,32 @@ let lessthan=x<y
 console.log(lessthan)
 let win
 console.log(win)
+let person =null;
+let emptyArray = []
+let numbers = [1,2,3]
+let randomArray = [1,"Best",true,null] 
+let myName = "Anita"
+console.log(myName)
+console.log(numbers)
+console.log(numbers[2])
+console.log(randomArray[3]) 
+console.log(numbers.length)
+console.log(randomArray.length)
+console.log(emptyArray.length)
+let newArray =[5,6,1,2,5,6,7,8,10,100,0,13]
+let lastitemposition=newArray.length-1
+console.log(newArray[lastitemposition])
+let newObject={}
+let personObject={
+    name:"Anita Egede",
+    age :13,
+    job :"Junior Software Developer",
+    isrich:true
+}
+console.log(personObject) 
+console.log(personObject.name)                                    
+console.log(personObject.age)
+console.log(personObject.job)
+console.log(personObject["name"])
+console.log(personObject["age"])
+console.log(personObject["job"])
